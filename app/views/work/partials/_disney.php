568f00f31e31f5cc5fc0b46efef96ab10e790db0
Al Scott <al.scott@agencyrepublic.com>
2013-11-27 15:29:14 +0000

    added a chevron to better demark interactive elements

--- app/views/work/partials/_disney.php
+++ app/views/work/partials/_disney.php
@@ -1,1 +1,5 @@
+<h2 id="intro" title="You like to read between the lines, huh?">
+	This was a project for Disney's Wreck it Ralph film, which was rather good by all accounts.
+</h2>
+
 <?php echo Lang::get('projects.disney_name'); ?>
--- app/views/work/show.php
+++ app/views/work/show.php
@@ -1,3 +1,1 @@
-SHOWING <?php echo $handle; ?>
-
 <?php echo View::make('work.partials._'.$handle); ?>
