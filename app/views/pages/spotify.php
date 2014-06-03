782e2d8559104cb250fb21ecac86aa473e6168d9
Al Scott <thealscott@gmail.com>
2014-06-03 15:36:22 +0100

    updated spotify page with latest version of bookmarklet and amended copy accordingly

--- app/views/pages/spotify.php
+++ app/views/pages/spotify.php
@@ -9,12 +9,12 @@
 	</h2>
 
 	<p>
-		For reasons unknown to mankind, HTTP links for Spotify <a href="http://open.spotify.com/user/alscott/playlist/4hFkuhNtPi07LgibIrXrg7" target="blank" title="HipHop-o-cratic Oath">(like&nbsp;this)</a> always try to open the web player, even if you have the native app. This bookmarklet lets you convert the HTTP url when the web player opens to a well formed Spotify URI so you can copy pasta it without&nbsp;hassle.
+		For reasons unknown to mankind, HTTP links for Spotify <a href="http://open.spotify.com/user/alscott/playlist/4hFkuhNtPi07LgibIrXrg7" target="blank" title="HipHop-o-cratic Oath">(like&nbsp;this)</a> always try to open the web player, even if you have the native app. This bookmarklet will convert the URL into a Spotify URI and attempt to open it in the native&nbsp;app.
 	</p>
 
 	<h3>Drag this link to your bookmarks</h3>
 
-	<a href="javascript:{var%20pathArray=window.location.pathname.split('/');var%20newPathname='spotify';for(i=1;i<pathArray.length;i++){newPathname+=':';newPathname+=pathArray[i];}window.prompt('Here%20is%20your%20Spotify%20URI:', newPathname);};void(0);" 
+	<a href="javascript:{var%20pathArray=window.location.pathname.split('/');var%20newPathname="spotify";for(i=1;i<pathArray.length;i++){newPathname+=":";newPathname+=pathArray[i];}window.location.replace(newPathname);};void(0);" 
 		class="button large_button"
 		title="Spotify URI">
 		Spotify URI
